e212d88cabfcefc99f4ed21ba520773401187577
Carlos Junio <cjunior1@live.com>
2023-05-24 21:35:50 -0300

    feat: refactor validations form

--- src/components/CardRegister/index.tsx
+++ src/components/CardRegister/index.tsx
@@ -8,40 +8,12 @@ import {
   Text,
 } from "@chakra-ui/react"
 import { motion } from "framer-motion"
-import { useForm, SubmitHandler, FieldValues } from "react-hook-form"
+import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { z } from "zod"
+
 import { useAuthContext } from "../../contexts/authContext"
-
-const registerSchema = z
-  .object({
-    fullName: z
-      .string()
-      .min(3, "Mínimo 3 caracteres.")
-      .max(128, "Máximo 128 caracteres."),
-    email: z
-      .string()
-      .email("Email inválido.")
-      .max(128, "Máximo 128 caracteres."),
-    phoneNumber: z
-      .string()
-      .min(11, "Mínimo de 11 dígitos.")
-      .max(11, "Máximo de 11 dígitos."),
-    password: z
-      .string()
-      .regex(/[A-Z]/, "Mínimo de 1 letra maiúscula.")
-      .regex(/[a-z]/, "Mínimo de 1 letra minuscula.")
-      .regex(/(\d)/, "Mínimo 1 número.")
-      .regex(/(\W)|_/, "Mínimo de 1 caractere especial.")
-      .regex(/(.{8,})|_/, "Mínimo de 8 caracteres."),
-    confirmPassword: z.string(),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "As senhas não correspondem.",
-    path: ["confirmPassword"],
-  })
-
-export type IRegisterData = z.infer<typeof registerSchema>
+import { RegisterForm } from "../../validations/auth"
+import { IRegisterForm } from "../../validations/types"
 
 interface ICardResgisterProps {
   toggleCard(card: string): void
@@ -54,13 +26,13 @@ const CardRegister = ({ toggleCard }: ICardResgisterProps) => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({
-    resolver: zodResolver(registerSchema),
+  } = useForm<IRegisterForm>({
+    resolver: zodResolver(RegisterForm),
   })
 
   return (
     <FormControl
-      onSubmit={handleSubmit(signUp as SubmitHandler<FieldValues>)}
+      onSubmit={handleSubmit(signUp)}
       isRequired
       as={motion.form}
       bg="gray.100"
