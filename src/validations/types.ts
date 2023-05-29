290355ce63d45c173a32b4fe16c08975fbc3d33b
Carlos Junio <cjunior1@live.com>
2023-05-29 20:36:59 -0300

    feat: types forms

--- src/validations/types.ts
+++ src/validations/types.ts
@@ -1,8 +1,11 @@
 import { z } from "zod"
-import { LoginForm, RegisterForm } from "./auth"
+import { LoginForm } from "./auth"
+import { RegisterForm, UpdateForm } from "./user"
 
 type ILoginForm = z.infer<typeof LoginForm>
 
 type IRegisterForm = z.infer<typeof RegisterForm>
 
-export type { ILoginForm, IRegisterForm }
+type IUpdateForm = z.infer<typeof UpdateForm>
+
+export type { ILoginForm, IRegisterForm, IUpdateForm }
