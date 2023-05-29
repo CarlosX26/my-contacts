63787f02260cd2798888762fcdf0a28f5481632b
Carlos Junio <cjunior1@live.com>
2023-05-29 20:36:41 -0300

    feat: user forms validation move

---
+++ src/validations/user.ts
@@ -0,0 +1,33 @@
+import { z } from "zod"
+
+const UserForm = z.object({
+  fullName: z
+    .string()
+    .min(3, "Mínimo 3 caracteres.")
+    .max(128, "Máximo 128 caracteres."),
+  email: z.string().email("Email inválido.").max(128, "Máximo 128 caracteres."),
+  phoneNumber: z
+    .string()
+    .min(11, "Mínimo de 11 dígitos.")
+    .max(11, "Máximo de 11 dígitos."),
+  password: z
+    .string()
+    .regex(/[A-Z]/, "Mínimo de 1 letra maiúscula.")
+    .regex(/[a-z]/, "Mínimo de 1 letra minuscula.")
+    .regex(/(\d)/, "Mínimo 1 número.")
+    .regex(/(\W)|_/, "Mínimo de 1 caractere especial.")
+    .regex(/(.{8,})|_/, "Mínimo de 8 caracteres."),
+  confirmPassword: z.string(),
+})
+
+const RegisterForm = UserForm.refine(
+  (data) => data.password === data.confirmPassword,
+  {
+    message: "As senhas não correspondem.",
+    path: ["confirmPassword"],
+  }
+)
+
+const UpdateForm = UserForm.omit({ confirmPassword: true }).partial()
+
+export { RegisterForm, UpdateForm }
