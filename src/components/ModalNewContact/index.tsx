3a7b289096bea77f276faf567f293001d467e993
Carlos Junio <cjunior1@live.com>
2023-06-01 20:36:20 -0300

    feat: form feedback errors

--- src/components/ModalNewContact/index.tsx
+++ src/components/ModalNewContact/index.tsx
@@ -7,6 +7,7 @@ import {
   ModalCloseButton,
   ModalContent,
   ModalHeader,
+  Text,
 } from "@chakra-ui/react"
 import { useContactContext } from "../../contexts/contactContext"
 import { useForm } from "react-hook-form"
@@ -15,23 +16,32 @@ import { motion } from "framer-motion"
 import { IRegisterContactForm } from "../../validations/types"
 import { RegisterContactForm } from "../../validations/contact"
 
-const ModalNewContact = () => {
+interface IModalNewContactProps {
+  onClose(): void
+}
+
+const ModalNewContact = ({ onClose }: IModalNewContactProps) => {
   const { newContact } = useContactContext()
 
-  const { register, handleSubmit } = useForm<IRegisterContactForm>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IRegisterContactForm>({
     resolver: zodResolver(RegisterContactForm),
   })
+
   return (
     <ModalContent mx="16px">
       <ModalHeader>Adicionar Contato</ModalHeader>
       <ModalCloseButton />
       <ModalBody>
         <FormControl
-          onSubmit={handleSubmit(newContact)}
+          onSubmit={handleSubmit((data) => newContact(data, onClose))}
           as={motion.form}
           display="flex"
           flexDir="column"
-          gap="16px"
+          gap="8px"
         >
           <FormLabel>Nome</FormLabel>
           <Input
@@ -39,18 +49,30 @@ const ModalNewContact = () => {
             placeholder="Digite o nome do contato."
             {...register("fullName")}
           />
+          <Text fontSize="x-small" color="red.400">
+            {errors.fullName?.message as string}
+          </Text>
+
           <FormLabel>Email</FormLabel>
           <Input
             type="text"
             placeholder="Digite o email do contato."
             {...register("email")}
           />
+          <Text fontSize="x-small" color="red.400">
+            {errors.email?.message as string}
+          </Text>
+
           <FormLabel>Telefone</FormLabel>
           <Input
             type="text"
             placeholder="Digite o telefone do contato."
             {...register("phoneNumber")}
           />
+          <Text fontSize="x-small" color="red.400">
+            {errors.phoneNumber?.message as string}
+          </Text>
+
           <Button type="submit">Cadastrar contato</Button>
         </FormControl>
       </ModalBody>
